7f5a4c44956e97a00c128e949767078e6f059987
caio <caiorajap@gmail.com>
2024-05-13 14:10:07 -0300

    Proj Calc

---
+++ ListaDeContatosT7/Contato.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ListaDeContatosT7
+{
+    internal class Contato
+    {
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        /*Propriedades (get e set)
+        é um controle de acesso ás variaveis. */
+
+        public string Nome { get { return nome; } set { nome = value; } }
+
+        public string Sobrenome { get { return sobrenome; } set { sobrenome = value; } }
+
+        public string Telefone
+        {
+            get
+            {
+
+                return telefone;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    //País-Cidade-Telefone, ex +55-11-79367-9051
+                    telefone = "-00-00000-0000";
+                }
+            }
+
+        }
+        //o método que tem o mesmo nome da classe
+        //e não retorna nada, é chamado CONSTRUTOR
+        //DA CLASSE
+        public void contato()
+        {
+            Nome = "José";
+            Sobrenome = "Da Silva";
+            Telefone = "11 99999-9999";
+        }
+
+        public void contato(string nome, string sobrenome, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+
+        //Sobreescreve o método ToString()
+        public override string ToString()
+        {
+            string saida = string.Empty;
+            saida += String.Format("{0}, {1}", Nome, Sobrenome);
+            saida += String.Format("({0}) {1}-{2}", Telefone.Substring(0, 2), Telefone.Substring(2, 5), Telefone.Substring(7, 4));
+            return saida;
+        }
+    }
+}
---
+++ ListaDeContatosT7/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ListaDeContatosT7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Contato[] contatos = new Contato[1];
+
+        private void escrever(Contato contato)
+        {
+            StreamWriter escreverEmArquivo = new StreamWriter("contato.txt");
+            escreverEmArquivo.WriteLine(contatos.Length + 1);
+            escreverEmArquivo.WriteLine(contato.Nome);
+            escreverEmArquivo.WriteLine(contato.Sobrenome);
+            escreverEmArquivo.WriteLine(contato.Telefone);
+
+            for (int x = 0; x < contatos.Length; x++)
+            {
+                escreverEmArquivo.WriteLine(contatos[x].Nome);
+                escreverEmArquivo.WriteLine(contatos[x].Sobrenome);
+                escreverEmArquivo.WriteLine(contatos[x].Telefone);
+            }
+
+            escreverEmArquivo.Close();
+        }
+        private void Escrever(Contato contato)
+        {
+            StreamWriter escreverEmArquivos = new StreamWriter("Contatos.txt");
+            escreverEmArquivos.WriteLine(contatos.Length + 1);
+            escreverEmArquivos.WriteLine(contato.Nome);
+            escreverEmArquivos.WriteLine(contato.Telefone);
+
+            for (int i = 0; i < contatos.Length; i++)
+            {
+                escreverEmArquivos.WriteLine(contatos[i].Nome);
+                escreverEmArquivos.WriteLine(contatos[i].Sobrenome);
+                escreverEmArquivos.WriteLine(contatos[i].Telefone);
+            }
+
+            escreverEmArquivos.Close();
+        }
+
+        private void Ler()
+        {
+            StreamReader lerArquivo = new StreamReader("Contatos.txt");
+            contatos = new Contato[Convert.ToInt32(lerArquivo.ReadLine())];
+
+            for (int x = 0; x < contatos.Length; x++)
+            {
+                contatos[x] = new Contato();
+                contatos[x].Nome = lerArquivo.ReadLine();
+                contatos[x].Sobrenome = lerArquivo.ReadLine();
+                contatos[x].Telefone = lerArquivo.ReadLine();
+            }
+            lerArquivo.Close();
+
+
+        }
+
+        private void Exibir()
+        {
+            listBoxContatos.Items.Clear();
+            for (int x = 0; x < contatos.Length; x++)
+            {
+                listBoxContatos.Items.Add(contatos[x].ToString());
+            }
+        }
+        private void LimparFormulario()
+        {
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+            textBox3.Text = String.Empty;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //criar um objeto de clasee contato
+            Contato contato = new Contato();
+            contato.Nome = textBox1.Text;
+            contato.Sobrenome = textBox1.Text;
+            contato.Telefone = textBox1.Text;
+
+            //listBoxContatos.Items.Add(contato.ToString());
+
+            Escrever(contato);
+            Organizar();
+            Ler();
+            Exibir();
+            LimparFormulario();
+        }
+        private void Organizar()
+        {
+            Contato temporário;
+            bool troca = true;
+
+            do
+            {
+                troca = false;
+                for (int x = 0; x < contatos.Length - 1;)
+                {
+                    if (contatos[x].Nome.CompareTo(contatos[x + 1].Nome) > 0)
+                    {
+                        temporário = contatos[x];
+                        contatos[x] = contatos[x + 1];
+                        contatos[x + 1] = temporário;
+                        troca = true;
+                    }
+                }
+            } while (troca == true);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Organizar();
+            Exibir
+        }
+    }
+
+}
